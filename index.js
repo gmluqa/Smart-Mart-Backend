1240b6bf909af615a93d405022a4d618d3264cb5
gmluqa <gmluqa@protonmail.com>
2022-12-21 11:42:10 +0200

    chore: changed the listening on consolelog, specified smart mart express server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 3000
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at localhost:${PORT}`)
+    console.log(`Smart Mart Express server listening at localhost:${PORT}`)
 })
 
 app.get("/hello", function (req, res) {
