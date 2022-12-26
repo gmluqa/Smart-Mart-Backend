d86ec4613eb5e485600de9e0251747b126f28c80
gmluqa <gmluqa@protonmail.com>
2022-12-26 08:48:01 +0200

    chore: migrates succesfully to prod on railway

--- config/config.js
+++ config/config.js
@@ -16,10 +16,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQLUSER,
+    "password": process.env.MYSQLPASSWORD,
+    "database": process.env.MYSQLDATABASE,
+    "port": process.env.MYSQLPORT,
+    "host": process.env.MYSQLHOST,
     "dialect": "mysql"
   }
 }
