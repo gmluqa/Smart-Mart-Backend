224d674975db786e8f4ee798b15af1c0417c8097
gmluqa <gmluqa@protonmail.com>
2022-12-29 11:00:31 +0200

    chore: made ids fixed for roles

--- seeders/20221228094700-demo-role.js
+++ seeders/20221228094700-demo-role.js
@@ -4,30 +4,35 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Roles', [{
+      id: 1,
       user_id: 1,
       role: "Admin",
       CreatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
       UpdatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
     },
     {
+      id: 2,
       user_id: 2,
       role: "User",
       CreatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
       UpdatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
     },
     {
+      id: 3,
       user_id: 3,
       role: "User",
       CreatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
       UpdatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
     },
     {
+      id: 4,
       user_id: 4,
       role: "User",
       CreatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
       UpdatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
     },
     {
+      id: 5,
       user_id: 5,
       role: "User",
       CreatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
