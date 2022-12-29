a2740011ced6872b8db790b81b386121e6d793aa
gmluqa <gmluqa@protonmail.com>
2022-12-29 10:55:43 +0200

    chore: dummy imgs done

---
+++ seeders/20221229085235-img.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('Imgs', [{
+      id: 1,
+      product_id: 1,
+      img_path: "/pathexample1",
+      img_definer: "/definerexample1",
+      createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      updatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
+    },
+    {
+      id: 2,
+      product_id: 2,
+      img_path: "/pathexample2",
+      img_definer: "/definerexample2",
+      createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      updatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
+    },
+    {
+      id: 3,
+      product_id: 3,
+      img_path: "/pathexample3",
+      img_definer: "/definerexample3",
+      createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      updatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
+    },
+    {
+      id: 4,
+      product_id: 4,
+      img_path: "/pathexample4",
+      img_definer: "/definerexample4",
+      createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      updatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
+    },
+    {
+      id: 5,
+      product_id: 5,
+      img_path: "/pathexample5",
+      img_definer: "/definerexample5",
+      createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+      updatedAt: Sequelize.literal("CURRENT_TIMESTAMP")
+    }], {});
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Imgs', null, {});
+  }
+};
