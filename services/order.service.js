8dd8139352fbef65fa64c5bc3a091e70e42bc515
gmluqa <gmluqa@protonmail.com>
2023-01-14 16:23:47 +0200

    chore: we get user by emails id

--- services/order.service.js
+++ services/order.service.js
@@ -1,12 +1,17 @@
 const models = require("../models/index");
 const crypto = require("crypto");
 const jsonwebtoken = require("jsonwebtoken");
+const { findUserByEmail } = require("../services/user.service");
 
 const createNewOrder = async (body, jwt) => {
   // find out what user it is, by decoding jwt
   jwt = jwt && jwt.split(" ")[1];
   jwt = jsonwebtoken.decode(jwt);
-  console.log(jwt);
+
+  let foundUserId = await findUserByEmail(jwt.email);
+  foundUserId = foundUserId.id;
+  console.log(foundUserId);
+  // get user by email endpoint
   // generate a constant order ID
   const orderUuid = crypto.randomUUID;
   // for each item, create an order with it, if such an item doesn't exist (not found, skip it)
