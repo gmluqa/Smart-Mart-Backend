7ccf01324fb7eb4d7ca75a4b42498a7d5408ced1
gmluqa <gmluqa@protonmail.com>
2023-01-14 15:34:50 +0200

    chore: route in app now

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ app.use(express.json());
 // Middleware for Routes
 const productRouter = require("./routers/product.router.js");
 const userRouter = require("./routers/user.router.js");
+const orderRouter = require("./routers/order.router.js");
 
 // Routes
 app.use("/product", productRouter);
 app.use("/user", userRouter);
+app.use("/order", orderRouter);
 
 module.exports = app;
