72aa7f2e5e8a7497b9579566bdb885e9ea331b10
gmluqa <gmluqa@protonmail.com>
2023-01-14 15:32:02 +0200

    chore: order router route set up

---
+++ routers/order.router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const orderRouter = express.Router();
+
+// const {} = require("../controllers/order.controller.js");
+
+// productRouter.get("/id/:id", getProductDetailController);
+
+module.exports = orderRouter;
