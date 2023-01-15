b372fc8c2d52f2b1bc206091038425eb40279c13
gmluqa <gmluqa@protonmail.com>
2023-01-15 17:26:54 +0200

    chore: seeders all have correct ids

--- seeders/20221229085235-img.js
+++ seeders/20221229085235-img.js
@@ -51,6 +51,141 @@ module.exports = {
           createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
           updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
         },
+        {
+          id: 6,
+          product_id: 6,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 7,
+          product_id: 7,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 8,
+          product_id: 8,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 9,
+          product_id: 9,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 10,
+          product_id: 10,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 11,
+          product_id: 11,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 12,
+          product_id: 12,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 13,
+          product_id: 13,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 14,
+          product_id: 14,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 15,
+          product_id: 15,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 16,
+          product_id: 16,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 17,
+          product_id: 17,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 18,
+          product_id: 18,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 19,
+          product_id: 19,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          id: 20,
+          product_id: 20,
+          img_path:
+            "https://ae01.alicdn.com/kf/Hbd7356f3b3974554a587bd310c0865f74/Rodillo-r-pido-para-hacer-Sushi-molde-de-arroz-utensilios-de-cocina-para-carne-y-verduras.jpg_Q90.jpg",
+          img_definer: "main",
+          createdAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updatedAt: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       ],
       {}
     );
