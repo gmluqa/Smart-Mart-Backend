f8caf12c9d1d4f0326c4aee8e95c74db311b0175
gmluqa <gmluqa@protonmail.com>
2023-01-20 16:58:28 +0200

    chore: chores defined

--- routers/product.router.js
+++ routers/product.router.js
@@ -8,9 +8,16 @@ const {
   searchProductDetailByNameController,
 } = require("../controllers/product.controller.js");
 
+// CREATE (only admin can create a new product)
+
+// READ (can be accessed by anyone, no auth needed)
 productRouter.get("/id/:id", getProductDetailController);
 productRouter.get("/id/:id/img/:imgDefiner", getImageByIdAndDefiner);
 productRouter.get("/name/:name", getProductDetailByNameController);
 productRouter.get("/search/:search", searchProductDetailByNameController);
+
+// UPDATE (only admin can update a product)
+
+// DELETE (only admin can delete a product)
 
 module.exports = productRouter;
